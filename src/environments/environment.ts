3845b1189ac852b08f7da97337ba1f81a8d67b7a
Suresh Natarajan <suresh.n.natarajan@gmail.com>
2020-05-19 09:30:07 +0530

    Changed endpoint and compile error for --prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  invest_api_endpoint: 'http://localhost:8090/vaanigaInvest/admin/',
-  //invest_api_endpoint: 'https://vaaniga-invest-api.herokuapp.com/vaanigaInvest/admin/',
+  //invest_api_endpoint: 'http://localhost:8090/vaanigaInvest/admin/',
+  invest_api_endpoint: 'https://vaaniga-invest-api.herokuapp.com/vaanigaInvest/admin/',
   invest_api_dashboard_endpoint: 'https://vaaniga-invest-api.herokuapp.com/vaanigaInvest/dashboard/',
   invest_api_nseoptions_endpoint: 'http://localhost:8090/vaanigaInvest/nseIndia/',
   vaaniga_signup_endpoint: 'https://vaaniga-invest-signup.herokuapp.com/vaanigaInvest/signUp/',
