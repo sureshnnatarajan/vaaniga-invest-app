dd06ad0e3502fe41e7ae5210d895835135cf6db0
Suresh Natarajan <suresh.n.natarajan@gmail.com>
2020-05-21 20:38:23 +0530

    Added learning glossary

--- src/app/views/listed-companies/listed-companies.component.ts
+++ src/app/views/listed-companies/listed-companies.component.ts
@@ -10,6 +10,9 @@ export class ListedCompaniesComponent implements OnInit {
   largeCapCompanies : any[] = [];
   midCapCompanies : any[] = [];
   smallCapCompanies : any[] = [];
+  notListedInBse : any[] = [];
+  notListedInNse : any[] = [];
+  listedInNseAndBse : any[] = [];
 
   largeCapCompaniesCount = 0;
   midCapCompaniesCount = 0;
@@ -34,18 +37,45 @@ export class ListedCompaniesComponent implements OnInit {
     results.forEach(element => {
       if (element.marketCapCategorization === 'Large Cap') {
         this.largeCapCompanies.push(element);
+        this.checkNseBseListed(element);
+        this.checkBseListed(element);
+        this.checkNseListed(element);
         this.largeCapCompaniesCount = this.largeCapCompaniesCount + 1;
       }
       if (element.marketCapCategorization === 'Mid Cap') {
         this.midCapCompanies.push(element);
+        this.checkNseBseListed(element);
+        this.checkBseListed(element);
+        this.checkNseListed(element);
         this.midCapCompaniesCount = this.midCapCompaniesCount + 1;
       }
       if (element.marketCapCategorization === 'Small Cap') {
         this.smallCapCompanies.push(element);
+        this.checkNseBseListed(element);
+        this.checkBseListed(element);
+        this.checkNseListed(element);
         this.smallCapCompaniesCount = this.smallCapCompaniesCount + 1;
       }
     });
-
+    //console.log("BSE Not Listed ==>" + JSON.stringify(this.notListedInBse));
+    //console.log("NSE Not Listed ==>" + JSON.stringify(this.notListedInNse));
   }
 
+  checkNseListed(element: any) {
+    if (element.nseSymbol.length <= 0) {
+      this.notListedInNse.push(element);
+    }
+  }
+
+  checkBseListed(element: any) {
+    if (element.bseSymbol.length <= 0) {
+      this.notListedInBse.push(element);
+    }
+  }
+
+  checkNseBseListed(element: any) {
+    if (element.nseSymbol.length >= 0 && element.bseSymbol.length >=0 ) {
+      this.listedInNseAndBse.push(element);
+    }
+  }
 }
